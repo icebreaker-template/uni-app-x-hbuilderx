1bda70c30fbf2eeda39929ede225f4699284e311
ice breaker <1324318532@qq.com>
2025-04-09 14:22:15 +0800

    chore(deps): upgrade

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,32 @@
 import { defineConfig } from "vite";
 import uni from "@dcloudio/vite-plugin-uni";
 import { UnifiedViteWeappTailwindcssPlugin } from 'weapp-tailwindcss/vite'
-
-export default defineConfig(async () => {
-  // 这里必须这样引用，因为 uni 只提供了 cjs 的版本且 uni-app 默认 cjs，而 @tailwindcss/vite 只提供了 esm 版本
-  const { default: tailwindcss } = await import('@tailwindcss/vite')
-  return {
+import tailwindcss from '@tailwindcss/postcss'
+export default defineConfig({
     plugins: [
-      uni(),
-      tailwindcss(),
-      UnifiedViteWeappTailwindcssPlugin(
-        {
-          rem2rpx: true,
-          tailwindcssPatcherOptions: {
-            patch: {
-              tailwindcss: {
-                v4: {
-                  base: __dirname
+        uni(),
+        UnifiedViteWeappTailwindcssPlugin(
+            {
+                rem2rpx: true,
+                tailwindcssPatcherOptions: {
+                    patch: {
+                        tailwindcss: {
+                            v4: {
+                                base: __dirname
+                            }
+                        }
+                    }
                 }
-              }
             }
-          }
-        }
-      ),
+        )
     ],
-  }
+    css: {
+        postcss: {
+            plugins: [
+                tailwindcss({
+                    base: __dirname
+                })
+            ]
+        }
+    }
 });
