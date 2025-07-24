823ac04605c6fd4702f918bb9a7ae7063c8a7a39
ice breaker <1324318532@qq.com>
2025-07-24 15:49:03 +0800

    chore(deps): upgrade

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { UnifiedViteWeappTailwindcssPlugin } from 'weapp-tailwindcss/vite'
 import { r } from './shared'
 import { uniAppX } from 'weapp-tailwindcss/presets'
 import tailwindcss from '@tailwindcss/postcss'
+// import { debugX } from '@weapp-tailwindcss/debug-uni-app-x'
 
 export default defineConfig({
     plugins: [
@@ -13,11 +14,11 @@ export default defineConfig({
                 base: __dirname,
                 rem2rpx: true,
                 cssEntries: [r(__dirname, 'main.css')],
-                resolve: {
-                    paths: [import.meta.url],
-                },
             }),
         ),
+        // debugX({
+        //     cwd: __dirname,
+        // })
     ],
     css: {
         postcss: {
