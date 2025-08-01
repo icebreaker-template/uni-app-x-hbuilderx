73e7bccd42207503348c5e53c85363c4e3f5f8c4
ice breaker <1324318532@qq.com>
2025-08-01 10:04:41 +0800

    chore(deps): upgrade

--- shared.js
+++ shared.js
@@ -4,6 +4,11 @@ function r(...args) {
   return path.resolve(__dirname, ...args)
 }
 
+const isH5 = process.env.UNI_PLATFORM === 'h5'
+const isApp = process.env.UNI_PLATFORM === 'app'
+
 module.exports = {
   r,
+  isH5,
+  isApp,
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,18 @@
 const { r } = require('./shared')
+const { addIconSelectors } = require("@iconify/tailwind")
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     r('./pages/**/*.{uts,uvue}'),
+    r('./components/**/*.{uts,uvue}'),
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    addIconSelectors(['svg-spinners', 'mdi']),
+  ],
   corePlugins: {
     preflight: false,
   },
