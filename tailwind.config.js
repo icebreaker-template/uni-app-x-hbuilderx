a361ea01885ae3c80b72e9c1c58fb3bc2ac0d112
ice breaker <1324318532@qq.com>
2025-08-26 00:42:21 +0800

    chore: upgrade

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ const { r } = require('./shared')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: ['class'],
   content: [
     r('./pages/**/*.{uts,uvue}'),
     r('./components/**/*.{uts,uvue}'),
@@ -10,7 +11,7 @@ module.exports = {
     extend: {},
   },
   plugins: [
-    
+
   ],
   corePlugins: {
     preflight: false,
